e816531f01a4d1a7d93de967390721906471120f
bayiastan <bsbaha195@gmail.com>
2026-03-03 19:21:21 +0600

    new hooks up

--- infra/docker/nginx-entrypoint.sh
+++ infra/docker/nginx-entrypoint.sh
@@ -1,6 +1,23 @@
 #!/bin/sh
-# Select nginx config by NGINX_CONF_TARGET (http.conf or ssl.conf), then run nginx.
 set -e
+
 TARGET="${NGINX_CONF_TARGET:-http.conf}"
-cp "/etc/nginx/conf.d/${TARGET}" /etc/nginx/conf.d/default.conf
-exec nginx -g 'daemon off;'
+echo "[entrypoint] NGINX_CONF_TARGET=$TARGET"
+
+# SSL суралса, бирок cert жок болсо http'ко түш
+if [ "$TARGET" = "ssl.conf" ]; then
+  if [ ! -f "/etc/letsencrypt/live/nuranpark.com/fullchain.pem" ] || \
+     [ ! -f "/etc/letsencrypt/live/nuranpark.com/privkey.pem" ]; then
+    echo "[entrypoint] SSL cert not found, falling back to http.conf"
+    TARGET="http.conf"
+  fi
+fi
+
+# conf.d ичинде бир гана файл болсун
+rm -f /etc/nginx/conf.d/*
+
+# TEMPLATE ПАПКАДАН көчүрөбүз
+cp "/etc/nginx/templates/$TARGET" /etc/nginx/conf.d/default.conf
+
+nginx -t
+exec nginx -g "daemon off;"
